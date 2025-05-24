a06a43161b62a28f185775284919b1afa118d5d7
Amulya Jain <amuly.jain@gmail.com>
2025-05-24 13:28:27 -0400

    fix: update return type of main function to HttpResponse and improve response format

--- api/app.py
+++ api/app.py
@@ -1,9 +1,10 @@
 import azure.functions as func
 import logging
+
 app = func.FunctionApp()
 
 @app.function_name(name="HttpTrigger1")
 @app.route(route="req")
-def main(req: func.HttpRequest) -> str:
+def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    return f"Hello, bill!"
+    return func.HttpResponse("Hello, bill!", status_code=200)
