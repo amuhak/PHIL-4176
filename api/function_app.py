428689072d9523ef13dc0564a32031b938a5f93d
Amulya Jain <amuly.jain@gmail.com>
2025-05-26 13:36:33 -0400

    Update

--- api/function_app.py
+++ api/function_app.py
@@ -1,13 +1,19 @@
 import azure.functions as func
 import logging
 import json
+import os
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="req")
 def req(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
+    try:
+        api_key = os.environ["GOOGLE_API_KEY"]
+        logging.info(f"API Key: {api_key}")
+    except Exception as e:
+        logging.error(f"Error retrieving API key: {e}")
+    
     try:
         # Attempt to get JSON from the request body
         body_dict = req.get_json()
